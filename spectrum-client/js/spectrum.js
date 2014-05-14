a292bbc566787e7a1bfa353fc1d88211ac43e907
Matthieu Imbert <matthieu.imbert@inria.fr>
2014-05-14 15:32:16 +0200

    dynamic scaling of x axis with the fft size

--- spectrum-client/js/spectrum.js
+++ spectrum-client/js/spectrum.js
@@ -1,9 +1,17 @@
 var chart = d3.select(".spectrum");
 var width = chart.attr("width");
 var height = chart.attr("height");
-var sx = d3.scale.linear()
-    .domain([0, 1024])
-    .rangeRound([0, width]);
+
+var fft_size = 1;
+var sx;
+function update_sx(size) {
+    sx = d3.scale.linear()
+        .domain([0, size])
+        .rangeRound([0, width]);
+}
+update_sx(fft_size);
+
+
 var sy = d3.scale.pow()
     .exponent(4)
     .domain([0, 255])
@@ -17,6 +25,10 @@ ws.onclose = function() { console.log("spectrum websocket closed"); };
 ws.onmessage = function(evt) {
     if (evt.data instanceof ArrayBuffer) {
         spectrum = new Uint8Array(evt.data);
+        if (spectrum.length != fft_size) {
+            fft_size = spectrum.length;
+            update_sx(fft_size);
+        }
 
         var updt = chart.selectAll("rect")
             .data(spectrum)
