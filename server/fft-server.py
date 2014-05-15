d07841e3663cf27cd12473f4c97a3169ee5e482e
Matthieu Imbert <matthieu.imbert@inria.fr>
2014-05-15 16:49:36 +0200

    bugfixes + optimization (frame segmentation done in udp listener microthread) + use a logger instead of print

--- server/fft-server.py
+++ server/fft-server.py
@@ -6,33 +6,80 @@ from geventwebsocket.handler import WebSocketHandler
 from bottle import request, Bottle, abort
 from traceback import format_exc
 from gevent import socket
-import struct, sys
+import struct, sys, logging, logging.handlers
+
+logger = logging.getLogger("fft-server")
+logger.setLevel("WARN")
+#logger.addHandler(logging.StreamHandler(sys.stdout))
+handler = logging.handlers.SysLogHandler("/dev/log")
+handler.setFormatter(logging.Formatter('%(name)s/%(levelname)s: %(message)s'))
+logger.addHandler(handler)
 
 GRSF_SYNC = "\xac\xdd\xa4\xe2\xf2\x8c\x20\xfc"
 GRSF_SYNC_LENGTH = len(GRSF_SYNC)
 GRSF_HEADER_LENGTH = GRSF_SYNC_LENGTH + 1
 
-class UdpSpectrumListener(object):
+class UdpSpectrumReceiver(object):
 
     def __init__(self, port):
         self._udpsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._udpsock.bind(("", port))
-        self.recvd = Event()
-        self.last_recv = None
-        self.last_recv_counter = None
-        spawn(self._udp_fetch)
+        self._workbuf = ""
+        self._packet_length = -1
+        self._simple_framer_seq_num = None
+        self.new_fft_available = Event()
+        self.last_fft_received = None
+        self.last_fft_received_count = None
+        spawn(self._udp_listen_and_segment)
 
-    def _udp_fetch(self):
+    def _udp_listen_and_segment(self):
         while True:
-            self.last_recv = self._udpsock.recv(32768)
-            if self.last_recv_counter == None:
-                self.last_recv_counter = 0
-            else:
-                self.last_recv_counter += 1
-            self.recvd.set()
-            self.recvd.clear()
+            self._workbuf += self._udpsock.recv(32768)
+            next_packet = -1
+            if self._packet_length != -1:
+                if len(self._workbuf) >= self._packet_length:
+                    if (self._workbuf[
+                            self._packet_length - GRSF_HEADER_LENGTH
+                            :self._packet_length - GRSF_HEADER_LENGTH + GRSF_SYNC_LENGTH]
+                        == GRSF_SYNC):
+                        next_packet = self._packet_length
+                        logger.debug("header found where expected, packet_length = %i" % (self._packet_length,))
+                    else:
+                        self._packet_length = -1
+                        logger.debug("header NOT found where expected -> will have to relearn packet length")
+                else:
+                    continue
+            if self._packet_length == -1:
+                next_packet = self._workbuf.find(GRSF_SYNC) + GRSF_HEADER_LENGTH
+                logger.debug("found header at %i" % (next_packet,))
+            if next_packet != -1:
+                payload = ""
+                if self._packet_length != -1:
+                    payload = self._workbuf[:next_packet - GRSF_HEADER_LENGTH - 1]
+                    # end is minus one because simple framer pads its
+                    # payload with one byte, see
+                    # gr-digital/include/gnuradio/digital/simple_framer_sync.h
+                    next_simple_framer_seq_num_str = self._workbuf[next_packet - GRSF_HEADER_LENGTH + GRSF_SYNC_LENGTH]
+                    next_simple_framer_seq_num, = struct.unpack("B", next_simple_framer_seq_num_str)
+                    if (self._simple_framer_seq_num != None and
+                        (self._simple_framer_seq_num + 1) % 256 != next_simple_framer_seq_num):
+                        logger.info("simple_framer_seq_num = %i, next_simple_framer_seq_num = %i" % (
+                            self._simple_framer_seq_num, next_simple_framer_seq_num))
+                    self._simple_framer_seq_num = next_simple_framer_seq_num
+                else:
+                    self._packet_length = next_packet
+                    logger.debug("learned packet length = %i" % (self._packet_length,))
+                self._workbuf = self._workbuf[next_packet:]
+                if len(payload) > 0:
+                    self.last_fft_received = payload
+                    if self.last_fft_received_count == None:
+                        self.last_fft_received_count = 0
+                    else:
+                        self.last_fft_received_count += 1
+                    self.new_fft_available.set()
+                    self.new_fft_available.clear()
 
-usl = UdpSpectrumListener(6663)
+udp_receiver = UdpSpectrumReceiver(6663)
 
 app = Bottle()
 
@@ -42,7 +89,7 @@ def request_id(request):
 @app.route('/spectrum')
 def handle_spectrum_ws():
     try:
-        print request_id(request) + ': handle_spectrum_ws start'
+        logger.info(request_id(request) + ': connection')
         wsock = request.environ.get('wsgi.websocket')
         if not wsock:
             abort(400, request_id(request) + ': Expected WebSocket request.')
@@ -50,59 +97,25 @@ def handle_spectrum_ws():
         workbuf = ""
         udp_counter = None
         packet_length = -1
-        seq_num = None
+        simple_framer_seq_num = None
 
         while not wsock.closed:
             try:
                 while True:
-                    usl.recvd.wait()
-                    if udp_counter == None or udp_counter < usl.last_recv_counter:
-                        udp_counter = usl.last_recv_counter
-                        workbuf += usl.last_recv
-                        break
-
-                next_packet = -1
-                if packet_length != -1:
-                    if len(workbuf) >= packet_length:
-                        if (workbuf[
-                                packet_length - GRSF_HEADER_LENGTH
-                                :packet_length - GRSF_HEADER_LENGTH + GRSF_SYNC_LENGTH]
-                            == GRSF_SYNC):
-                            next_packet = packet_length
-                        else:
-                            packet_length = -1
-                    else:
-                        continue
-                else:
-                    next_packet = workbuf.find(GRSF_SYNC) + GRSF_HEADER_LENGTH
-                if next_packet != -1:
-                    payload = ""
-                    if packet_length != -1:
-                        payload = workbuf[:next_packet - GRSF_HEADER_LENGTH - 1]
-                        # end is minus one because simple framer pads its
-                        # payload with one byte, see
-                        # gr-digital/include/gnuradio/digital/simple_framer_sync.h
-                        next_seq_num_str = workbuf[next_packet - GRSF_HEADER_LENGTH + GRSF_SYNC_LENGTH]
-                        next_seq_num, = struct.unpack("B", next_seq_num_str)
-                        if (seq_num != None and
-                            (seq_num + 1) % 256 != next_seq_num):
-                            print >> sys.stderr, request_id(request) + ": seq_num = %i, next_seq_num = %i" % (
-                                seq_num, next_seq_num)
-                        seq_num = next_seq_num
-                    workbuf = workbuf[next_packet:]
-                    packet_length = next_packet
-                    if len(payload) > 0:
-                        wsock.send(payload, binary = True)
-                        payload = ""
+                    udp_receiver.new_fft_available.wait()
+                    if udp_counter == None or udp_counter < udp_receiver.last_fft_received_count:
+                        udp_counter = udp_receiver.last_fft_received_count
+                        wsock.send(udp_receiver.last_fft_received, binary = True)
             except WebSocketError:
-                print request_id(request) + ": WebSocketError"
+                logger.debug(request_id(request) + ": WebSocketError")
                 break
-        print request_id(request) + ": websocket closed"
+        logger.info(request_id(request) + ": websocket closed")
     except Exception, exc:
-        print >> sys.stderr, request_id(request) + ": exception occured in handle_spectrum_ws:\n%s" % (
-                format_exc(),)
+        logger.error(request_id(request) +
+                     ": exception occured in handle_spectrum_ws:\n%s" % (
+                format_exc(),))
     finally:
-        print request_id(request) + ": handle_spectrum_ws end"
+        logger.info(request_id(request) + ": end")
 
 server = WSGIServer(("0.0.0.0", 8080), app,
                     handler_class=WebSocketHandler)
