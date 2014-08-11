430e3a6df759aca4f8e3f5e43ca45851c4969205
Matthieu Imbert <matthieu.imbert@inria.fr>
2014-08-11 15:57:26 +0200

    major fixes and improvements: - use signed chars everywhere (grc hier block output and fft js input) - add power_min / power_max and related graph scaling - linear svg scale since log power fft block already outputs in log scale - no svg y ticks anymore, we don't know the scale

--- client/js/fft.js
+++ client/js/fft.js
@@ -17,14 +17,13 @@ function Fft(elem, host, port, udpport) {
         .attr("transform", "translate(" + this._margin.left + "," + this._margin.top + ")");
     this._current_fft_size = -1;
     this._sx = null;
-    this._sy = d3.scale.pow()
-        .exponent(4)
-        .domain([0, 255])
+    this._sy = d3.scale.linear()
+        .domain([-128,127])
         .rangeRound([0, this._graph_height]);
     this._yaxis = d3.svg.axis()
         .scale(this._sy)
-        .tickValues([0, 192, 256])
-        .tickFormat(function(d) { return ((d / 256.0) * -100.0).toFixed(0); })
+        .tickValues([])
+        .tickFormat(function(d) { return (((d+128.0) / 256.0) * -100.0).toFixed(0); })
         .orient("left");
     this._svgxaxis = this._chart.append("g")
         .attr("class", "x axis")
@@ -65,7 +64,7 @@ Fft.prototype.start = function() {
     var self = this;
     this._ws.onmessage = function(evt) {
         if (evt.data instanceof ArrayBuffer) {
-            fft = new Uint8Array(evt.data);
+            fft = new Int8Array(evt.data);
             self._update_sx(fft.length);
             self._fftpath.attr("d", self._fftline(fft));
         }
